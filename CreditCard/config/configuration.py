68b7f7901f766fe1f5f3abb1f815b638d46c728e
SachinPitale <sachinpitale22@gmail.com>
2022-07-24 11:43:44 +0530

    added get training pipeline config

--- CreditCard/config/configuration.py
+++ CreditCard/config/configuration.py
@@ -1,5 +1,5 @@
 from re import T
-from CreditCard.entity.config_entity import TrainingPipelineConfig
+from CreditCard.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
 from CreditCard.constant import *
 from CreditCard.exception import CreditCardException
 import os, sys
@@ -29,5 +29,52 @@ class Configuration:
             training_pipeline_config = TrainingPipelineConfig(artifact_dir=artifact_dir)
             logging.info(f"Training pipleine config: {training_pipeline_config}")
             return training_pipeline_config
+        except Exception as e:
+            raise CreditCardException(e,sys) from e
+    
+    def get_data_ingestion_config(self)-> DataIngestionConfig:
+        try:
+            # getting artifact directory
+            artifact_dir = self.training_pipeline_config.artifact_dir
+
+            #getting data ingestion directory inside artifact directory
+            data_ingestion_artifact_dir=os.path.join(artifact_dir,
+                DATA_INGESTION_ARTIFACT_DIR,
+                self.time_stamp
+                )
+            # retrive all data ingestion info from config.yaml 
+            data_ingestion_info = self.config_info[DATA_INGESTION_CONFIG_KEY]
+            
+            # get source data url
+            dataset_download_url = data_ingestion_info[DATA_INGESTION_DOWNLOAD_URL_KEY]
+
+            # destination location to download file
+            tgz_download_dir = os.path.join(data_ingestion_artifact_dir,
+                data_ingestion_info[DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY]
+            )
+            # main directory, when we keep our row data inside artifact
+            raw_data_dir = os.path.join(data_ingestion_artifact_dir,
+                data_ingestion_info[DATA_INGESTION_RAW_DATA_DIR_KEY]
+            )
+
+            # directory inside we keep train and test data
+            ingested_data_dir =  os.path.join(data_ingestion_artifact_dir,
+            data_ingestion_info[DATA_INGESTION_INGESTED_DIR_NAME_KEY]
+                )
+            # directory for keep train data
+            ingested_train_dir = os.path.join(ingested_data_dir,
+            data_ingestion_info[DATA_INGESTION_TRAIN_DIR_KEY]
+            )
+
+            ingested_test_dir = os.path.join(ingested_data_dir,
+            data_ingestion_info[DATA_INGESTION_TEST_DIR_KEY]
+            )
+
+            DataIngestionConfig(
+            dataset_download_url=dataset_download_url,
+            tgz_download_dir=tgz_download_dir,
+            raw_data_dir=raw_data_dir,
+            ingested_train_dir=ingested_train_dir,
+            ingested_test_dir=ingested_test_dir)
         except Exception as e:
             raise CreditCardException(e,sys) from e
