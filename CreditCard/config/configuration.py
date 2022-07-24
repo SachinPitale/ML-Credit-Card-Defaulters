35b7c43a3ed3e866f1ff049ca9b61e870e198f7c
SachinPitale <sachinpitale22@gmail.com>
2022-07-24 12:17:27 +0530

    added data ingestion model

--- CreditCard/config/configuration.py
+++ CreditCard/config/configuration.py
@@ -70,11 +70,13 @@ class Configuration:
             data_ingestion_info[DATA_INGESTION_TEST_DIR_KEY]
             )
 
-            DataIngestionConfig(
+            data_ingestion_config= DataIngestionConfig(
             dataset_download_url=dataset_download_url,
             tgz_download_dir=tgz_download_dir,
             raw_data_dir=raw_data_dir,
             ingested_train_dir=ingested_train_dir,
             ingested_test_dir=ingested_test_dir)
+            logging.info(f"Data Ingestion config: {data_ingestion_config}")
+            return data_ingestion_config
         except Exception as e:
             raise CreditCardException(e,sys) from e
