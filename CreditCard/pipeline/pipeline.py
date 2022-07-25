d00935205e93e06a6e77a9e0427f6cd2ab33a8aa
SachinPitale <sachinpitale22@gmail.com>
2022-07-25 15:04:47 +0530

    added data validation python file

--- CreditCard/pipeline/pipeline.py
+++ CreditCard/pipeline/pipeline.py
@@ -4,9 +4,9 @@ import os,sys
 from CreditCard.logger import logging
 from CreditCard.component.data_ingestion import DataIngestion
 from CreditCard.config.configuration import Configuration
-from CreditCard.entity.artifcat_entity import DataIngestionArtifact
+from CreditCard.entity.artifcat_entity import DataIngestionArtifact,DataValidationArtifact
 from CreditCard.entity.config_entity import DataIngestionConfig
-
+from CreditCard.component.data_validation import Datavalidation
 
 class Pipeline:
     
@@ -23,9 +23,20 @@ class Pipeline:
             return data_ingestion.initiate_data_ingestion()
         except Exception as e:
             raise CreditCardException(e,sys) from e
+    
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact) -> DataValidationArtifact:
+        try:
+            data_validation = Datavalidation(data_validation_config=self.config.get_data_validation_config(),
+                                              data_ingestion_artifact=data_ingestion_artifact)
+            return data_validation.initiate_data_validation()
+        except Exception as e:
+            raise CreditCardException(e,sys) from e
 
     def run_pipeline(self):
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            logging.info("started data valiadtion")
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            logging.info("completed data valiadtion")
         except Exception as e:
             raise CreditCardException(e,sys) from e
